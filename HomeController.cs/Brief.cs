1a9b9d50506ac163a492b1fed31a6ccee00ea58d
Gurleen Kaur <200538494@student.georgianc.on.ca>
2023-09-20 21:25:02 -0400

    Html code

---
+++ HomeController.cs/Brief.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OneDrive.Documents.georgian college.Semester-3.Server-side.comp-2084-f23-assignment.HomeController.cs
+{
+    public class Brief
+    {
+     // Add action logic here
+    return View();
+    }
+}
+
---
+++ wwwroot/css/Brief.cshtml.cs
@@ -0,0 +1,92 @@
+using System.Drawing;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OneDrive.Documents.georgian college.Semester-3.Server-side.comp-2084-Assignments.wwwroot.css
+{
+    public class Brief.cshtml.css
+    {
+        
+    }
+}
+
+html {
+  font-size: 14px;
+}
+
+@media (min-width: 768px) {
+  html {
+    font-size: 16px;
+  }
+}
+
+.btn:focus, .btn:active:focus, .btn-link.nav-link:focus, .form-control:focus, .form-check-input:focus {
+  box-shadow: 0 0 0 0.1rem white, 0 0 0 0.25rem #258cfb;
+}
+
+html {
+  position: relative;
+  min-height: 100%;
+}
+
+body {
+  margin-bottom: 60px;
+}
+
+table {
+  border: 5px solid red;
+}
+
+/* Defining Brand Colors */
+:root {
+    --primary-color: #90ccc6; 
+    --secondary-color: #47a447; 
+    --accent-color: #ff6c60; 
+}
+
+/* Using Brand Colors */
+body {
+    background-color: var(--primary-color);
+    color: #fff; /* Text color on the primary background */
+}
+
+.header {
+    background-color: var(--secondary-color);
+    color: #fff; /* Text color on the secondary background */
+    padding: 10px;
+}
+
+.button {
+    background-color: var(--accent-color);
+    color: #fff; /* Text color on the accent background */
+    border: none;
+    padding: 10px 20px;
+    cursor: pointer;
+}
+
+/* Add more CSS rules for different elements and components as needed */
+/* Title */
+h1 {
+  font-family: 'Cedarville Cursive', cursive;
+  font-size: 2.5rem; 
+  color: var(--primary-color);
+  margin-bottom: 20px; 
+}
+
+/* Subtitle */
+h2 {
+  font-family: 'Dancing Script', cursive;
+  font-size: 1.8rem; 
+  color: var(--secondary-color);
+  margin-top: 20px; 
+}
+
+/* Body Text */
+p {
+  font-family: 'Satisfy', cursive; 
+  font-size: 1rem; 
+  color: #333; 
+  line-height: 1.4; 
+}
